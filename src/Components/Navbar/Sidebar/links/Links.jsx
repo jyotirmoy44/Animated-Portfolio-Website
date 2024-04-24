4e938f348844c19fe2954323bc89a8a7847a52c9
Jyotirmoy Sharma <sharmajyotirmoy44@gmail.com>
2024-04-24 10:09:13 +0530

    Commit from Office

--- src/Components/Navbar/Sidebar/links/Links.jsx
+++ src/Components/Navbar/Sidebar/links/Links.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const Links = () => {
+
+  const items = [
+    "Homepage",
+    "Services",
+    "Portfolio",
+    "Contact",
+    "About",
+  ]
   return (
-    <div>Links</div>
+    <div className='links'>
+      {items.map((item) => (
+        <a href={`#${item}`} key={item}>
+          {item}
+        </a>
+      ))}
+    </div>
   )
 }
 
